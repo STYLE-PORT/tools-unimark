8f0180615db0a24b433d340e8ebcb2c7b1a7afbb
yukimi-inu <yuki.u.clover@gmail.com>
2025-03-07 19:29:11 +0900

    fix: base path (local / production)

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import react from "@vitejs/plugin-react-swc";
 import {defineConfig} from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "./tools-unimark/",
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly'
+export default defineConfig(({mode}) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/tools-unimark/' : '/',
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly'
+      }
     }
-  }
+  };
 });
