7e4f375612428195c14606539fddc6a3fe27d222
yukimi-inu <yuki.u.clover@gmail.com>
2025-03-07 19:22:18 +0900

    fix: base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "./tools-unimark/",
   css: {
     modules: {
       localsConvention: 'camelCaseOnly'
